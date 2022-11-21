97b0e848b6ddd8bf7df906a675788c0d9cd65d1b
abdul <abdul.isegen@gmail.com>
2022-11-21 11:09:40 -0500

    added functionality

--- src/services/users-service.js
+++ src/services/users-service.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 const BASE_URL = "https://fse2node.herokuapp.com/api";
 // const BASE_URL = "http://localhost:4000/api";
-
-const LOGIN_API = `${BASE_URL}/login`;
+const TUITS_API = `${BASE_URL}/tuits`;
 const USERS_API = `${BASE_URL}/users`;
 
 export const createUser = (user) =>
